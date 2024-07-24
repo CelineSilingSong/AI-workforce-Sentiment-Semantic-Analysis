f95403dac06b46f639375ce0e7d2f6d12f175284
CelineSilingSong <ss4799@princeton.edu>
2024-07-24 11:23:52 -0400

    extra adding

---
+++ AEI AI Sentiment Analysis/# Semantic Analysis.py
@@ -0,0 +1,11 @@
+# Semantic analysis and Sentiment analysis:
+
+
+# Semantic analysis block:
+from bertopic import BERTopic
+from sklearn.datasets import fetch_20newsgroups
+
+
+
+
+# Sentiment analysis block:
