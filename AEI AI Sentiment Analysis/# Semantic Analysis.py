756492d214d4ac15a3268623818f08bbf1ccbbb6
CelineSilingSong <ss4799@princeton.edu>
2024-07-24 11:44:11 -0400

    semantic analysis

--- AEI AI Sentiment Analysis/# Semantic Analysis.py
+++ AEI AI Sentiment Analysis/# Semantic Analysis.py
@@ -1,11 +1,32 @@
-# Semantic analysis and Sentiment analysis:
+# Semantic analysis and Sentiment analysis (English Version):
+import os
+import pandas as pd
+
+# preprocessor block:
+Directory = 'en-macroed'
+
+files = []
+for file in os.listdir(Directory):
+    if file.endswith('.csv'):
+        file_path = os.path.join(Directory,file)
+        files.append(file_path)
+
+sorted_files = sorted(files)
+
+for file in sorted_files:
+    df = pd.read_csv(file)
+    
 
 
 # Semantic analysis block:
 from bertopic import BERTopic
-from sklearn.datasets import fetch_20newsgroups
 
 
 
 
 # Sentiment analysis block:
+
+
+
+
+# cleaning the dataset:
